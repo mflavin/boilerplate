8eee6277c134e41560439f9f5d921437810b6c80
mflavin <mflavin@hawk.iit.edu>
2024-10-04 16:09:35 -0500

    chore: removing starter / default vue js files

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,12 +1,21 @@
-import { ref, computed } from 'vue'
-import { defineStore } from 'pinia'
+import { ref, computed } from 'vue';
+import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+  const count = ref(0);
+  const doubleCount = computed(() => count.value * 2);
 
-  return { count, doubleCount, increment }
-})
+  const $reset = () => {
+    count.value = 0;
+  };
+
+  const decrement = () => {
+    count.value--;
+  };
+
+  const increment = () => {
+    count.value++;
+  };
+
+  return { count, doubleCount, $reset, decrement, increment };
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { storeToRefs } from 'pinia';
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore();
+const { count, doubleCount } = storeToRefs(counterStore);
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Boilerplate using modern tools</h1>
+  <p>Counter: {{ count }}</p>
+  <p>Double Counter: {{ doubleCount }}</p>
+  <button @click="counterStore.increment">Increment</button>
+  <button @click="counterStore.decrement">Decrement</button>
+  <button @click="counterStore.$reset">Reset</button>
 </template>
